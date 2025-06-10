4b17e1755ae4b169dd672b9c61bfd9a18df82d78
Esther Candido <esthercandido21@gmail.com>
2025-06-10 18:17:58 +0100

    Implementação -> class abstract

--- Program.cs
+++ Program.cs
@@ -1,3 +1,15 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Smartphone nk = new Nokia(numero: "123456", modelo: "Modelo I", imei: "IGHDSB23", memoria: 128);
+Smartphone iph = new Iphone(numero: "245678", modelo: "Modelo XR", imei: "34DHFD45", memoria: 256);
+
+Console.WriteLine("=========Smartphone Nokia=========");
+nk.Ligar();
+nk.ReceberLigacao();
+nk.InstalarAplicativo("Telegram");
+
+
+Console.WriteLine("\n========Smartphone Iphone=========");
+iph.Ligar();
+iph.ReceberLigacao();
+iph.InstalarAplicativo("Duolingo");
